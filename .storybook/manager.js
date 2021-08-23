14123a0e394be5b1ed7c202cf5a0c2be7a242541
Carlos Santos <carlos.santos74@outlook.com>
2021-08-23 10:34:47 -0300

    Finaliza a config

---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/addons';
+import custonTheme from './theme';
+
+
+addons.setConfig({
+  theme: custonTheme
+})
---
+++ .storybook/theme.js
@@ -0,0 +1,31 @@
+import { create } from '@storybook/theming';
+
+export default create({ 
+  
+  brandTitle: 'inBolso component book',
+  brandUrl: 'https://www.inbolso.com.br/',
+  brandImage: 'https://www.inbolso.com.br/imgs/inBolso.svg',
+  
+  
+  base: 'light',
+  appBg: '#f0f1f5',
+
+  colorPrimary: '#00c65e',
+  
+  colorSecondary: '#00c65e',
+  appBorderColor: '#e4e5eb',
+  appBorderRadius: 4,
+  
+  fontBase: '"Poppins", sans-serif',
+  textColor: '#3c3f47',
+  textInverseColor: '#fff',
+
+  barTextColor: '#3c3f47',
+  barSelectedColor: '#00c65e',
+
+   
+  inputBg: '#fafbfc',
+  inputBorder: '#e4e5eb',
+  inputTextColor: '#3c3f47',
+  inputBorderRadius: 4,
+})
