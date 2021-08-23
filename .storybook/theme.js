76278bf45250c6c116150c5ded9aef839f394ced
Carlos Santos <carlos.santos74@outlook.com>
2021-08-23 13:22:43 -0300

    Adiciona home temporária

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,21 +1,20 @@
 import { create } from '@storybook/theming';
+import brandImage from '../src/stories/assets/logo.svg';
 
-export default create({ 
-  
-  brandTitle: 'inBolso component book',
+export default create({
+  brandTitle: 'inBolso',
   brandUrl: 'https://www.inbolso.com.br/',
-  brandImage: 'https://www.inbolso.com.br/imgs/inBolso.svg',
-  
-  
+  brandImage,
+
   base: 'light',
   appBg: '#f0f1f5',
 
   colorPrimary: '#00c65e',
-  
+
   colorSecondary: '#00c65e',
   appBorderColor: '#e4e5eb',
   appBorderRadius: 4,
-  
+
   fontBase: '"Poppins", sans-serif',
   textColor: '#3c3f47',
   textInverseColor: '#fff',
@@ -23,9 +22,8 @@ export default create({
   barTextColor: '#3c3f47',
   barSelectedColor: '#00c65e',
 
-   
   inputBg: '#fafbfc',
   inputBorder: '#e4e5eb',
   inputTextColor: '#3c3f47',
   inputBorderRadius: 4,
-})
+});
